28bd0481a34609495c99805718fa1c8617ba2f8d
skhotyanovsky <s.khotyanovsky@gmail.com>
2023-02-17 09:11:44 +0200

    Example005

---
+++ Example005_CondotionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша")
+{
+    Console.WriteLine("Ура, это Маша");
+}
+else
+{
+    Console.Write("Привет,   ");
+    Console.WriteLine(username);
+}
